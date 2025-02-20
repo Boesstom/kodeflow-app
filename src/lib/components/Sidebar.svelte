b23be78619425b608124620e884971fb04fa9e7b
Boesstom <boesstom@gmail.com>
2025-02-20 09:20:03 +0700

    fixing sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -181,18 +181,19 @@
           href="/student/courses"
           class="flex items-center px-2 py-2 text-sm font-medium rounded-md hover:bg-gray-100 text-gray-600 hover:text-gray-900"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-          />
-        </svg>
-        {#if !isCollapsed}
-          <span class="ml-3">Projects</span>
-        {/if}
-      </a>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+            />
+          </svg>
+          {#if !isCollapsed}
+            <span class="ml-3">Projects</span>
+          {/if}
+        </a>
+      {/if}
 
       <!-- Settings -->
       <a
